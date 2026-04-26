3293435704fd014ce2acfcddbab695dc6d2d00fe
Janhvi04 <janhavi004goy@gmail.com>
2026-04-26 11:57:57 +0530

    Updated validation and fixes

--- Contact/form.py
+++ Contact/form.py
@@ -5,3 +5,9 @@ class Contactform(forms.ModelForm):
     class Meta:
         model=Contact
         fields=["first_name","last_name","address","email","phone_no"]
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if Contact.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email already exists")
+        return email
